fbe1e1b875d4fd07570f3336a91de8841534e3fe
Ginta <ginta.aizstrauta@gmail.com>
2022-11-04 13:14:37 +0200

    feat: added standart function for calculator

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ class Calculator {
     }
 
     delete() {
-
+        this.currentOperand = this.currentOperand.toString().slice(0, -1)
     }
 
     appendNuber(number) {
@@ -29,55 +29,98 @@ class Calculator {
         this.previousOperand = this.currentOperand
         this.currentOperand = ''
     }
-    
+
+    negateNumber() {
+        
+        }
+        
     compute() {
-        let computation
+        let result
         const prev = parseFloat(this.previousOperand)
         const current = parseFloat(this.currentOperand)
         if (isNaN(prev) || isNaN(current)) return
         switch (this.operation) {
             case '+':
-                computation = prev + current
+                result = prev + current
                 break
 
             case '-':
-                computation = prev - current
+                result = prev - current
                 break
 
             case '*':
-                computation = prev * current
+                result = prev * current
                 break
 
             case '÷':
-                computation = prev / current
+                if (current === '0') {
+                    prev = ''
+                    current = ''
+                    operation = ''
+                    return 
+                }
+                result = prev / current
                 break
 
-          /*  case '^':
-                computation = prev - current
+            case 'x^':
+                result = prev ** current
                 break
 
-            case '&#8730':
-                computation = Math.sqrt(number)
-                break */
+            case '&radic;x':
+                result = Math.sqrt(numberButtons)
+                break
+
+            case '&plus;/&minus;':
+                result = -result
+                break
+
             default:
                 return                       
         }
-        this.currentOperand = computation
+        this.currentOperand = result
         this.operation = undefined
         this. previousOperand = ''
     }
 
+    getDisplayNumber(number) {
+        const stringNumber = number.toString()
+        const integerDigits = parseFloat(stringNumber.split('.')[0])
+        const decimalDigits = stringNumber.split('.') [1]
+        let integerDisplay
+       
+        if (isNaN(integerDigits)) {
+            integerDisplay = ''
+        } else {
+            integerDisplay = integerDigits.toLocaleString('en', {maximumFractionDigits: 0})
+        }
+        if (decimalDigits != null) {
+            return `${integerDisplay}.${decimalDigits}`
+        } else {
+            return integerDisplay
+        }
+    }
+
     updateDisplay(){
-        this.currentOperandTextElement.innerText = this.currentOperand
-        this.previousOperandTextElement.innerText = this.previousOperand
+        this.currentOperandTextElement.innerText = 
+        this.getDisplayNumber(this.currentOperand)
+        if (this.operation != null) {
+            this.previousOperandTextElement.innerText =
+        `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`
+        } else {
+            this.previousOperandTextElement.innerText = ''
+        }
     }
 }
+    
+    
+
 
 const numberButtons = document.querySelectorAll('[data-number]')
 const operationButtons = document.querySelectorAll('[data-operation]')
 const equalsButton = document.querySelector('[data-equals]')
 const deleteButton = document.querySelector('[data-delete]')
 const allClearButton = document.querySelector('[data-all-clear]')
+const negationButton = document.querySelector('[data-negation]')
 const previousOperandTextElement = document.querySelector('[data-previous-operand]')
 const currentOperandTextElement = document.querySelector('[data-current-operand]')
 
@@ -98,12 +141,23 @@ operationButtons.forEach(button => {
     })
 })
 
-equalsButton.addEventListener('click', button => {
+equalsButton.addEventListener('click', () => {
     calculator.compute()
     calculator.updateDisplay()
 })
 
-allClearButton.addEventListener('click', button => {
+allClearButton.addEventListener('click', () => {
     calculator.clear()
     calculator.updateDisplay()
 })
+
+deleteButton.addEventListener('click', () => {
+    calculator.delete()
+    calculator.updateDisplay()
+})
+
+negationButton.addEventListener('click', () => {
+    calculator.negateNumber()
+    calculator.updateDisplay()
+})
+
