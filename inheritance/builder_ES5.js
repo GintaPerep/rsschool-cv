4e09d8b89e21728da2db45a507aabff6530553bd
Ginta <ginta.aizstrauta@gmail.com>
2022-12-14 22:46:25 +0200

    feat: first functions in Builder_ES5

--- inheritance/builder_ES5.js
+++ inheritance/builder_ES5.js
@@ -1,26 +1,32 @@
-function Builder2(_initNum = 0) {
 
-    this.initNum = _initNum;
-    this.sum2 =  this.initNum ;
- }
+  
+  function BuilderES5 (greeting) {
+    this.greeting = greeting
+    this.text = ''
+  }
+  
+  BuilderES5.prototype.plus = function (...str) {
+    
+    for (let arg of str)
+       this.text += arg;
+       this.text = this.greeting + this.text
+    console.log(`'${this.text}'`)
+  }
 
-Builder2.prototype.plus = function(...n) { 
-    this.sum2
+  BuilderES5.prototype.minus = function (n) {
+       let result = this.text.substring(0, this.text.length - n)
+    
+    console.log(`'${result}'`)
+  }
 
-    for (let arg of n) this.sum2 += arg;
-    console.log(`Builder2 ${this.sum2}`)
-    return this.sum;
-};
+  function StringBuilder(greeting) {
+    BuilderES5.call(this, greeting);
+}
 
-Builder2.prototype.minus = function(...n) { 
-    this.sum2;
+StringBuilder.prototype = Object.create(BuilderES5.prototype);
 
-    for (let arg of n) this.sum2 -= arg;
-    console.log(`Builder2 ${this.sum2}`)
-    return this.sum2;
-};
+let strBuilder = new StringBuilder('Hello', 22);
+strBuilder.plus(' all','!')
+strBuilder.minus(4)
 
 
-let builder = new Builder2(10)
-builder.plus(2,3,2)
-builder.minus(1,2);
